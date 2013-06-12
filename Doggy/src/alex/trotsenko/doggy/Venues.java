aca7167f76a825182bfb00bb45fd811f4973318c
AlexTrotsenko <alexey_tr@list.ru>
2013-06-12 13:02:25 +0300

    Add initial Android project and stub sources

---
+++ Doggy/src/alex/trotsenko/doggy/Venues.java
@@ -0,0 +1,25 @@
+package alex.trotsenko.doggy;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Venues extends Activity
+{
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState)
+   {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_venues);
+   }
+
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu)
+   {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.venues, menu);
+      return true;
+   }
+
+}
